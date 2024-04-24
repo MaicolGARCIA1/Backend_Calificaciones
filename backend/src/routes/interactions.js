f29d7699829ad2143b66f7e3f9ccebb46474754d
MaicolGARCIA1 <115556588+MaicolGARCIA1@users.noreply.github.com>
2024-04-24 11:51:05 -0500

    Cambios  en el backen previamente

--- backend/src/routes/interactions.js
+++ backend/src/routes/interactions.js
@@ -4,15 +4,15 @@ const db = require('../db');
 
 // Route to save an interaction
 router.post('/save-rating', (req, res) => {
-  const { id_estudiante, tipo_interaccion, comentario, calificacion } = req.body;
+  const { id_estudiante, tipo_interaccion, comentario, calificacion, sede } = req.body;
 
   // Verifica si todos los campos necesarios están presentes en la solicitud
   if (!id_estudiante || !tipo_interaccion || !calificacion) {
     return res.status(400).send('Missing required fields');
   }
 
-  const query = 'INSERT INTO Interacciones (id_estudiante, tipo_interaccion, comentario, calificacion) VALUES (?, ?, ?, ?)';
-  db.query(query, [id_estudiante, tipo_interaccion, comentario, calificacion], (err, result) => {
+  const query = 'INSERT INTO Interacciones (id_estudiante, tipo_interaccion, comentario, calificacion, sede) VALUES (?, ?, ?, ?, ?)';
+  db.query(query, [id_estudiante, tipo_interaccion, comentario, calificacion, sede], (err, result) => {
     if (err) {
       console.error('Error saving interaction: ' + err.message);
       return res.status(500).send('Error saving interaction');
@@ -22,26 +22,22 @@ router.post('/save-rating', (req, res) => {
   });
 });
 
-module.exports = router;
-
+module.exports = router;
 // const express = require('express');
 // const router = express.Router();
 // const db = require('../db');
 
 // // Route to save an interaction
 // router.post('/save-rating', (req, res) => {
-//   const { id_estudiante, tipo_interaccion, comentario, calificacion } = req.body;
+//   const { id_estudiante, tipo_interaccion, comentario, calificacion, sede } = req.body;
 
 //   // Verifica si todos los campos necesarios están presentes en la solicitud
-//   if (!id_estudiante || !tipo_interaccion || !calificacion) {
+//   if (!id_estudiante || !tipo_interaccion || !calificacion || !sede) {
 //     return res.status(400).send('Missing required fields');
 //   }
 
-  
-
-
-//   const query = 'INSERT INTO Interacciones (id_estudiante, tipo_interaccion, comentario, calificacion) VALUES (?, ?, ?, ?)';
-//   db.query(query, [id_estudiante, tipo_interaccion, comentario, calificacion], (err, result) => {
+//   const query = 'INSERT INTO Interacciones (id_estudiante, tipo_interaccion, comentario, calificacion, sede) VALUES (?, ?, ?, ?, ?)';
+//   db.query(query, [id_estudiante, tipo_interaccion, comentario, calificacion, sede], (err, result) => {
 //     if (err) {
 //       console.error('Error saving interaction: ' + err.message);
 //       return res.status(500).send('Error saving interaction');
@@ -52,3 +48,5 @@ module.exports = router;
 // });
 
 // module.exports = router;
+
+
